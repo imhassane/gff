4933b287e49311eac9ad2fc84b20c3ce4099b88c
TH Sow <imthassane@gmail.com>
2019-08-08 17:23:40 +0200

    Correction de quelques bugs dont le hachage du mot de passe, l'affichage du mot de passe dans le type User et l'ajout des variables is_active & is_staff

--- server/models/user.js
+++ server/models/user.js
@@ -7,9 +7,9 @@ const UserSchema = new mongoose.Schema({
     email: { type: String, required: true },
     password: { type: String, required: true },
     picture: { type: mongoose.Schema.Types.ObjectId, ref: "Picture" },
+    is_staff: { type: Boolean, default: false },
+    is_active: { type: Boolean, default: false },
 }, { timestamps: true });
-
-const User = mongoose.model('User', UserSchema);
 
 UserSchema.pre('save', function() {
     this.username = this.username.toLowerCase();
@@ -20,11 +20,15 @@ UserSchema.methods.comparePasswords = function(other) {
     return bcrypt.compareSync(other, this.password);
 }
 
+const User = mongoose.model('User', UserSchema);
+
 const validateUser = (data) => Joi.validate(data, {
     username: Joi.string().required().min(5),
     email: Joi.string().email().required(),
     password: Joi.string().required(),
-    picture: Joi.string()
+    picture: Joi.string(),
+    is_staff: Joi.boolean(),
+    is_active: Joi.boolean(),
 });
 
 module.exports = { User, validateUser };
