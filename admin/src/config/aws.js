d926d9e03b850660f48fbf1ae947ae00ed91dbca
Hassane Sow <imthassane@Gmail.com>
2019-11-01 16:33:55 +0100

    Update aws.js

--- admin/src/config/aws.js
+++ admin/src/config/aws.js
@@ -2,7 +2,7 @@ export default {
     bucketName: 'sylla-msc',
     dirName: 'test',
     region: 'eu-west-3',
-    accessKeyId: 'AKIAJQ5MISGJESRNP5PA',
-    secretAccessKey: '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',
+    accessKeyId: process.env.ACCESS_KEY,
+    secretAccessKey: process.env.SECRET_KEY,
     /* https://sylla-msc.s3.eu-west-3.amazonaws.com/ */
 };
